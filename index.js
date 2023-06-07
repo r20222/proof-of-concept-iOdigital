5cf5fa414f3c9d7babc2220166fc9a350e38783b
r20222 <101579892+r20222@users.noreply.github.com>
2023-06-07 15:22:39 +0200

    core functionality
    ik heb alle basis html inhoud erin gezet

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ console.log('mooi')
 
 import express from 'express'
 
+// onderstaande code is om require te kunnen gebruiken
+import { createRequire } from 'module';
+
+
+const require = createRequire(import.meta.url);
 // Maak een nieuwe express app
 const app = express()
+
+
+// fs is een nodemodule die ervoor zorgt dat je bij je locale bestanden kunt
+const fs = require('fs');
+
+const jsonData = fs.readFileSync('beau-ter-ham.json');
+const data = JSON.parse(jsonData);
+
 
 // Stel in hoe we express gebruiken
 app.set('view engine', 'ejs')
@@ -12,7 +25,11 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-      response.render('index')
+    // Read the JSON file
+    console.log("bliep")
+    console.log(data);
+      response.render('index', { data: data});
+  
   })
 
 // Stel het poortnummer in en start express
@@ -20,3 +37,10 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+
+// async function fetchJson(jsonData) {
+//   return await fetch(jsonData)
+//     .then((response) => response.json())
+//     .catch((error) => error)
+// }
